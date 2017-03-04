9716ee39760200d00ca02102b82cfd35690a35c5
Superstar64 <Hexagonalstar64@gmail.com>
2017-03-04 00:28:36 -0500

    update file's header copyright

--- source/codegen.d
+++ source/codegen.d
@@ -1,17 +1,20 @@
-/+Copyright (C) 2015  Freddy Angel Cubas "Superstar64"
+/+
+	This file is part of Typi.
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, version 3 of the License.
+	Typi is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	Typi is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with Typi.  If not, see <http://www.gnu.org/licenses/>.
 +/
+module codegen;
 import std.array : join;
 import std.stdio : write;
 import std.conv : to;
